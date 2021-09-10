3bae4c98ab093b85a2f0be6a12369e06bc46a1a0
María García de Lomana <maria@redradix.com>
2021-09-10 12:26:29 +0200

    add base tests

--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-const bla = require('./index')
+const digPow = require("./index");
 
-describe('Bishop movement', () => {
-  it('fails', () => {
-    expect(bla.ble()).toBe(2)
-  })
-})
+describe("Playing with numbers", () => {
+  it("it solves the example cases", () => {
+    expect(digPow(89, 1)).toBe(1);
+    expect(digPow(91, 1)).toBe(-1);
+    expect(digPow(695, 2)).toBe(2);
+    expect(digPow(46288, 3)).toBe(51);
+  });
+});
