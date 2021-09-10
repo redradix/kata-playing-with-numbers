059fa6a450d2f0e3789a317543228f26ef96a3e2
Aarón Contreras <aaron@redradix.com>
2021-09-10 12:47:13 +0200

    Improve tests

--- test.js
+++ test.js
@@ -1,10 +1,13 @@
 const digPow = require("./index");
 
 describe("digPow", () => {
-  it("should return k", () => {
+  it("should return an integer k that satisfies the equation", () => {
     expect(digPow(89, 1)).toBe(1);
+    expect(digPow(695, 2)).toBe(2);
+    expect(digPow(46288, 3)).toBe(51);
   });
-  it("should return -1", () => {
+
+  it("should return -1 if no such integer k exists", () => {
     expect(digPow(92, 1)).toBe(-1);
     expect(digPow(93, 1)).toBe(-1);
   });
