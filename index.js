d9df9806cb4bb72930212087e77c613aff75fd5b
María García de Lomana <maria@redradix.com>
2021-09-10 12:40:20 +0200

    Add a numberToDigits helper function

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-module.exports = {
-  ble: () => 1
-}
+const numberToDigits = (n, accDigits = []) => {
+  if (n < 10) return [n, ...accDigits];
+
+  const units = n % 10; // 9
+  const tens = Math.trunc(n / 10); // 8
+
+  return numberToDigits(tens, [units, ...accDigits]);
+};
+
+const digPow = (n, p) => {
+  const digits = numberToDigits(n);
+
+  const k = -1;
+  return k;
+};
+
+module.exports = digPow;
