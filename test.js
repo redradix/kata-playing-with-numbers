9f6cf5369627a0f56542810529472110c4d9bc4a
Andres Martin <monkeyandres@protonmail.com>
2021-09-10 12:27:49 +0200

    Red for Jose Miguel

--- test.js
+++ test.js
@@ -4,4 +4,8 @@ describe('Playing with numbers', () => {
   it('works', () => {
     expect(true).toBe(true)
   })
+
+  it('returns -1 when find no k', () => {
+    expect(digPow(8, 1)).toEqual(1)
+  })
 })
