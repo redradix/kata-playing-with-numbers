e40ca6540178ab8f38689b786aab7753e3a641b5
Victor <victor@redradix.com>
2021-09-10 14:02:09 +0200

    Todo en uno

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-module.exports = {
-  ble: () => 1
+const bigPow = (n, p) => {
+  const sum = String(n)
+    .split('')
+    .map(d => Number(d))
+    .map((d, i) => Math.pow(d, i + p))
+    .reduce((sum, d) => sum + d)
+
+  return sum % n === 0 ? sum / n : -1
 }
+
+module.exports = bigPow
--- test.js
+++ test.js
@@ -1,7 +1,10 @@
-const bla = require('./index')
+const bigPow = require('./index')
 
 describe('Bishop movement', () => {
   it('fails', () => {
-    expect(bla.ble()).toBe(2)
+    expect(bigPow(89, 1)).toBe(1)
+    expect(bigPow(92, 1)).toBe(-1)
+    expect(bigPow(695, 2)).toBe(2)
+    expect(bigPow(46288, 3)).toBe(51)
   })
 })
