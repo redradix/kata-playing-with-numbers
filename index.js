1902cd4ae86b26ce44bc97efb770f5896ea100e1
Juan Redradix <juanportillo@redradix.com>
2021-09-10 12:37:28 +0200

    substract array of integer digits

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 module.exports = {
-  ble: () => 1
-}
+  ble: () => 1,
+};
+
+module.exports = {
+  digPow: (integer, p) => {
+    const integerArray = `${integer}`.split("").map((int) => Number(int));
+    return integerArray;
+  },
+};
