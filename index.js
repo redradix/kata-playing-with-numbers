01a7e7d54b241609264b0b83c7e1b8dfb8e4845b
María García de Lomana <maria@redradix.com>
2021-09-10 13:01:31 +0200

    Add a sumPow helper function

--- index.js
+++ index.js
@@ -7,8 +7,18 @@ const numberToDigits = (n, accDigits = []) => {
   return numberToDigits(tens, [units, ...accDigits]);
 };
 
+const sumPows = (digits, p) =>
+  digits.reduce((acc, digit, index) => {
+    const exponent = p + index;
+    const pow = Math.pow(digit, exponent);
+
+    return acc + pow;
+  }, 0);
+
 const digPow = (n, p) => {
   const digits = numberToDigits(n);
+
+  const sumOfPows = sumPows(digits, p);
 
   const k = -1;
   return k;
